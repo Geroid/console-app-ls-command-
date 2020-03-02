2617c829ab6caa04d0eb93ac65a74ff075a6bbf0
Viktor Rezvantsev <geroid1735@gmail.com>
2020-03-02 11:28:31 +0300

    add project

---
+++ Console app ls command/Console app ls command/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Console app ls command
+//
+//  Created by Viktor Rezvantsev on 02.03.2020.
+//  Copyright © 2020 Viktor Rezvantsev. All rights reserved.
+//
+
+import Foundation
+
+
+
